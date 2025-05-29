75a8f0966e1a19640576987c474a10ffed45e434
Brendan <brendan@Brendans-MacBook-Pro.local>
2025-05-28 20:49:10 -0600

    updated const for .c source char arrays

--- test/05/main.c
+++ test/05/main.c
@@ -2,10 +2,18 @@
 //#include <gb/hardware.h>
 //#include <rand.h>
 #include <stdio.h>
-#include "guy.h"
-#include "admiral_benbow_outside.h"
+#include "admiral_benbow_outside_62_tiles.h"
 #include "all_tiles.h"
+
+// ******** temp (testing different data) *********
 //#include "windowmap.c"
+#include "guy.h"
+#include "test_char.h"
+#include "examplesprite.h"
+#include "BackgroundData.h"
+#include "BackgroundMap.h"
+
+// ******** temp *********
 
 //use this custom delay function, instead of built in gbdk function
 // (less CPU spikes than built in gbdk funciton)
@@ -15,30 +23,30 @@ void performantdelay(uint8_t numloops){
         wait_vbl_done();
     }
 }
-
 void init(){
 
     set_bkg_data(0,7,all_tiles); // 7 tiles into background
-    set_bkg_tiles(0,0,90,90,adm_ben_out); // coords (0,0) and 90 tiles wide, 90 tiles high
+    set_bkg_tiles(0,0,90,90,adm_ben_out_62_tiles); // coords (0,0) and 90 tiles wide, 90 tiles high
     //set_win_tiles(0,0,5,1,windowmap);
 
-    set_sprite_data(0,7,guy_sprite);
+    set_sprite_data(0,2,guy);
     set_sprite_tile(0,0);
-    move_sprite(0, 88, 78);
-
+    move_sprite(0, 88, 78); // orig
+    //move_sprite(0,22,78);
 }
 
 void update_switches(){
-
-    DISPLAY_ON; // display on
 
     NR52_REG = 0x8F; // sound on
     NR51_REG = 0x11; // sound channels on
     NR50_REG = 0x77; // volume max
 
     HIDE_WIN;  //not sure what this does
-    SHOW_BKG;
     SHOW_SPRITES;
+    SHOW_BKG;
+    
+
+    //DISPLAY_ON; // display on
 }
 
 void check_input(){
@@ -90,7 +98,10 @@ void main(){
     
     //scrolling the background
     while(1){
+        scroll_bkg(1,0);
         check_input();
         delay(100);
     }
 }
+
+
